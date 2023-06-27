a61d9f624102b191fc57864b748cd6e079a0b4fb
Ali Tahoon <73113143+alitahoon@users.noreply.github.com>
2023-06-27 16:32:26 +0300

    apply MVP arch

---
+++ app/src/main/java/com/example/shoshaplaystation/ui/HomeContracts.kt
@@ -0,0 +1,4 @@
+package com.example.shoshaplaystation.ui
+
+interface HomeContracts {
+}
---
+++ app/src/main/java/com/example/shoshaplaystation/ui/splash.kt
@@ -0,0 +1,12 @@
+package com.example.shoshaplaystation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shoshaplaystation.R
+
+class splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
